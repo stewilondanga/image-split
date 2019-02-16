7e3206e6a476e68c7c0e2cf420fd97c934bf0800
Stewart ilondanga <stilondanga@gmail.com>
2019-02-16 16:00:26 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
-
+/*
 Splitting({
 	target: "[data-rows], [data-columns], [data-image]",
 	by: "cells",
